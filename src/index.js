24c87bfec460ef72408b2e1bf27055d082c777a8
WebSeed <Will.Bamford@sainsburys.co.uk>
2018-10-25 13:34:12 +0100

    Export a button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import React from 'react'
-import hello from './hello'
-import MyComponent from './MyComponent.jsx'
-
-const World = () => hello(<MyComponent />)
-
-export default World
+export * from './components/button'
